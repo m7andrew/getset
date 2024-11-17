3f9914b76115068aa28a33653c1b153f58719c53
Andrew <m7andrew@icloud.com>
2024-11-16 17:31:10 -0800

    Initial commit

---
+++ readme.md
@@ -0,0 +1,111 @@
+
+# Derive Getters, Setters, and Builders
+
+A **minimal** and opinionated Rust library for deriving getters, setters, and builders.
+
+## Installation
+
+To use in your project, add `getset` as a dependency to your `Cargo.toml` file:
+
+```toml
+[dependencies]
+getset = { git = "https://github.com/m7andrew/getset" }
+```
+
+## Getters & Setters
+
+Getters and setters are useful when you want to control the API of a struct without exposing its fields directly. To keep things simple, the `GetSet` derive macro uses only two attributes: `#[get]` and `#[set]`.
+
+```rust
+#[derive(Debug, GetSet)]
+pub struct Movie {
+  #[get] title:   String,
+  #[set] year:    u32,
+  #[set] runtime: u32,
+  #[set] genres:  Vec<String>,
+}
+```
+
+- `#[set]` derives both a getter and setter function for the associated field.
+- `#[get]` derives only a getter function for the associated field. 
+
+By default, a field without either attribute derives nothing.
+
+```rust
+movie.year()         // Get
+movie.set_year(2004) // Set
+```
+
+Setter functions can also be chained together:
+
+```rust
+movie
+  .set_year(2004)
+  .set_runtime(126)
+```
+
+If you need a setter without a getter, then it's best to implement the function manually. Typically in this situation, some amount of custom logic or validation is needed.
+
+## Builder
+
+The builder pattern is not a replacement for named arguments, but rather, it's a useful pattern when you need to construct a type that possesses several optional or default values.
+
+```rust
+#[derive(Debug, Default, Builder)]
+pub struct Movie {
+  title:   String,
+  year:    u32,
+  runtime: u32,
+  genres:  Vec<String>,
+}
+```
+
+The `Builder` macro derives a builder struct that wraps the original. However, the implementation of one or more constructors is left to you. This provides flexibility, letting you write constructors that require certain inputs or preform custom logic. The simplest constructor would wrap the default constructor:
+
+```rust
+impl Movie {
+  pub fn new() -> MovieBuilder {
+    MovieBuilder(Self::default())
+  }
+}
+```
+```rust
+let movie = Movie::new()
+  .title   ("Lord of the Rings".into())
+  .year    (2001)
+  .runtime (228)
+  .build   ();
+```
+
+## Customize Your Builder
+
+If needed, you can implement custom functions on the builder itself. Take the following example:
+
+```rust
+#[derive(Debug, Default, Builder)]
+pub struct Rect {
+  x:      i32,
+  y:      i32,
+  width:  i32,
+  height: i32,
+}
+
+impl Rect {
+  pub fn new(width: i32, height: i32) -> RectBuilder {
+    RectBuilder(Self { width, height, ..Default::default() })
+  }
+}
+
+impl RectBuilder {
+  pub fn position(mut self, x: i32, y: i32) -> Self {
+    self.0.x = x;
+    self.0.y = y;
+    self
+  }
+}
+```
+```rust
+let shape = Rect::new(12, 12)
+  .position(1, 3)
+  .build();
+```
---
+++ src/lib.rs
@@ -0,0 +1,129 @@
+#![allow(unused_braces)]
+
+use proc_macro  :: { TokenStream };
+use proc_macro2 :: { TokenStream as TokenStream2 };
+use syn         :: { Data, DeriveInput, Field, Fields, FieldsNamed };
+use syn         :: { parse_macro_input };
+use quote       :: { quote, format_ident };
+
+//-----------------------------------------------------------------------------
+//  Derive Macros
+//-----------------------------------------------------------------------------
+
+#[proc_macro_derive(GetSet, attributes(get, set))]
+pub fn derive_get_set(input: TokenStream) -> TokenStream {
+
+	let ast           = parse_macro_input!(input as DeriveInput);
+	let fields        = get_fields(&ast);
+	let struct_name   = &ast.ident;
+
+	let generics      = &ast.generics.split_for_impl();
+	let impl_generics = &generics.0;
+	let type_generics = &generics.1;
+	let where_clause  = &generics.2;
+
+	let getters       = fields.named.iter().map(derive_getter_function);
+	let setters       = fields.named.iter().map(derive_setter_function);
+
+	TokenStream::from(quote! {
+		#[allow(dead_code)]
+		impl #impl_generics #struct_name #type_generics #where_clause {
+			#(#getters)*
+			#(#setters)*
+		}
+	})
+}
+
+#[proc_macro_derive(Builder)]
+pub fn derive_builder(input: TokenStream) -> TokenStream {
+
+	let ast           = parse_macro_input!(input as DeriveInput);
+	let fields        = get_fields(&ast);
+	let visibility    = &ast.vis;
+	let struct_name   = &ast.ident;
+	let builder_name  = format_ident!("{struct_name}Builder");
+
+	let generics      = &ast.generics.split_for_impl();
+	let impl_generics = &generics.0;
+	let type_generics = &generics.1;
+	let where_clause  = &generics.2;
+
+	let builders      = fields.named.iter().map(derive_builder_function);
+
+	TokenStream::from(quote! {
+
+		#[derive(Debug)]
+		#visibility struct #builder_name(#struct_name);
+
+		#[allow(dead_code)]
+		impl #impl_generics #builder_name #type_generics #where_clause {
+			pub fn build(self) -> #struct_name { self.0 }
+			#(#builders)*
+		}
+	})
+}
+
+//-----------------------------------------------------------------------------
+//  Helper Functions
+//-----------------------------------------------------------------------------
+
+fn derive_builder_function(field: &Field) -> TokenStream2 {
+
+	let field_name = &field.ident.as_ref().unwrap();
+	let field_type = &field.ty;
+
+	quote! {
+		pub fn #field_name(mut self, value: #field_type) -> Self {
+			self.0.#field_name = value;
+			self
+		}
+	}
+}
+
+fn derive_getter_function(field: &Field) -> TokenStream2 {
+
+	if !has_attribute(&field, "get")
+	&& !has_attribute(&field, "set") { return quote!() }
+
+	let field_name = &field.ident.as_ref().unwrap();
+	let field_type = &field.ty;
+
+	quote! {
+		pub fn #field_name(&self) -> &#field_type {
+			&self.#field_name
+		}
+	}
+}
+
+fn derive_setter_function(field: &Field) -> TokenStream2 {
+
+	if !has_attribute(&field, "set") { return quote!() }
+
+	let field_name = &field.ident.as_ref().unwrap();
+	let field_type = &field.ty;
+	let func_name  = format_ident!("set_{field_name}");
+
+	quote! {
+		pub fn #func_name(&mut self, value: #field_type) -> &mut Self {
+			self.#field_name = value;
+			self
+		}
+	}
+}
+
+fn get_fields(ast: &DeriveInput) -> &FieldsNamed {
+
+	let Data::Struct(ref data) = ast.data else {
+		panic!("Can't derive functions from a non-struct");
+	};
+
+	let Fields::Named(ref fields) = data.fields else {
+		panic!("Can't derive functions from unnamed fields");
+	};
+
+	return fields
+}
+
+fn has_attribute(field: &Field, name: &'static str) -> bool {
+	field.attrs.iter().any(|attr| attr.path().is_ident(name))
+}
